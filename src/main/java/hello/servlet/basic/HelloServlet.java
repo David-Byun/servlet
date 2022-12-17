cf52b04475e4da694afbc865be17c19f473ecfa7
David-Byun <byundavid@naver.com>
2022-12-17 11:02:46 +0900

    hello servlet

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -2,15 +2,23 @@ package hello.servlet.basic;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        printStartLine(request, response);
+        printHeader(request);
+
+    }
+
+    private static void printStartLine(HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.println("HelloServlet.service");
         System.out.println("request = " + request);
         System.out.println("response = " + response);
@@ -21,5 +29,28 @@ public class HelloServlet extends HttpServlet {
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
         response.getWriter().write("hello " + username);
+
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println(cookie.getName() + ": " + cookie.getValue());
+            }
+        }
+
     }
+
+    //Header 모든 정보
+    private void printHeader(HttpServletRequest request) {
+        System.out.println("Header Start");
+        request.getHeaderNames().asIterator()
+                .forEachRemaining(headerName -> System.out.println("headerName = " + headerName));
+
+        request.getHeader("name");
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            System.out.println(headerName);
+        }
+    }
+
 }
